02a2c528a868d6f37062f66e7346c767f026e877
Ivan Briseno <ibriseno@mail.sfsu.edu>
2018-09-15 16:19:01 -0700

    The power operator has the lowest priority with a return value of 3. We could not use Math.pow() since it we have a conflict of double to int conversion. A separate statement was implemented to function as the power operator.

--- calculator/src/main/java/edu/csc413/calculator/operators/PowerOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/PowerOperator.java
@@ -3,12 +3,21 @@ package edu.csc413.calculator.operators;
 import edu.csc413.calculator.evaluator.Operand;
 
 public class PowerOperator extends Operator {
+    @Override
     public int priority(){
-        return 2;
+        return 3;
     }
 
+    @Override
     public Operand execute(Operand op1, Operand op2 ){
-        Operand temp = new Operand(op1.getValue() ^ op2.getValue());
+        Operand temp = new Operand(power(op1.getValue(), op2.getValue()));
         return temp;
+    }
+    public int power(int i, int j){
+        int result = i;
+        for(int c = 2 ; c <= j; c++){
+            result = result*i;
+
+        }return result;
     }
 }
