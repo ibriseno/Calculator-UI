d58b9347d4f0569c0339776e81d3ee48edf5a7c4
Ivan Briseno <ibriseno@mail.sfsu.edu>
2018-09-15 16:17:12 -0700

    Added override and return statement now gives the operator its priority.

--- calculator/src/main/java/edu/csc413/calculator/operators/MultiplyOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/MultiplyOperator.java
@@ -3,10 +3,12 @@ package edu.csc413.calculator.operators;
 import edu.csc413.calculator.evaluator.Operand;
 
 public  class MultiplyOperator extends Operator {
+    @Override
     public int priority(){
         return 2;
     }
 
+    @Override
     public Operand execute(Operand op1, Operand op2 ){
         Operand temp = new Operand(op1.getValue() * op2.getValue());
         return temp;
