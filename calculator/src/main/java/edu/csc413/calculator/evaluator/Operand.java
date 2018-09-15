d93eb1e84a515aad59a0d98999aabe86bb9bf79e
Ivan Briseno <ibriseno@mail.sfsu.edu>
2018-09-15 16:27:31 -0700

    Created a variable opnd to hold values for the Operand class.

--- calculator/src/main/java/edu/csc413/calculator/evaluator/Operand.java
+++ calculator/src/main/java/edu/csc413/calculator/evaluator/Operand.java
@@ -4,29 +4,42 @@ package edu.csc413.calculator.evaluator;
  * in a valid mathmetical expression.
  */
 public class Operand {
+  private int opnd;
   /**
   * construct operand from string token.
-  */  
+  */
   public Operand( String token ) {
-    
+    try{
+      opnd = Integer.parseInt(token);
+    }catch(NumberFormatException e){
+      System.out.println("Error 1");
+      System.exit(1);
+    }
+
   }
   /**
    * construct operand from integer
    */
   public Operand( int value ) {
+    opnd = value;
     
   }
   /**
    * return value of opernad
    */
   public int getValue() {
-      return 0;
+      return opnd;
   }
   /**
    * Check to see if given token is a valid
    * operand.
    */
   public static boolean check( String token ) {
-    return false;
+    try{
+      Integer.parseInt(token);
+    }catch(NumberFormatException e){
+      return false;
+    }
+    return true;
   }
 }
