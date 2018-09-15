e3a37bb246ae06e2bcc28b4188e5b12b718b0500
Ivan Briseno <ibriseno@mail.sfsu.edu>
2018-09-15 01:49:17 -0700

    No change was done

--- calculator/src/main/java/edu/csc413/calculator/operators/AddOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/AddOperator.java
@@ -5,7 +5,7 @@ import edu.csc413.calculator.evaluator.Operand;
 public class AddOperator extends Operator {
 
     public int priority(){
-        return 2;
+        return 0;
     }
 
    public Operand execute(Operand op1, Operand op2 ){
