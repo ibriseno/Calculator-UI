709209c10d7bc1185f2f8c62b99b3e853b952230
Ivan Briseno <ibriseno@mail.sfsu.edu>
2018-09-15 16:28:19 -0700

    Added Override to bypass parent class.

--- calculator/src/main/java/edu/csc413/calculator/operators/SubtractOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/SubtractOperator.java
@@ -3,10 +3,12 @@ package edu.csc413.calculator.operators;
 import edu.csc413.calculator.evaluator.Operand;
 
 public class SubtractOperator extends Operator {
+    @Override
     public int priority(){
-        return 2;
+        return 1;
     }
 
+    @Override
     public Operand execute(Operand op1, Operand op2 ){
         Operand temp = new Operand(op1.getValue() - op2.getValue());
         return temp;
