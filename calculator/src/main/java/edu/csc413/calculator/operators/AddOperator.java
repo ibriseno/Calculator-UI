7e3db8b160f699ba09ee9ff3d0127c2df21e9bde
Ivan Briseno <ibriseno@mail.sfsu.edu>
2018-09-15 16:15:50 -0700

    The Override was needed to bypass the parent class operator where we were calling it from. The return statement on the priority is the value of the priorrity to the given operator, in this case we return a value of one, which is our highest order.

--- calculator/src/main/java/edu/csc413/calculator/operators/AddOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/AddOperator.java
@@ -3,14 +3,14 @@ package edu.csc413.calculator.operators;
 import edu.csc413.calculator.evaluator.Operand;
 
 public class AddOperator extends Operator {
-
+    @Override
     public int priority(){
-        return 0;
-    }
 
+        return 1;
+    }
+    @Override
    public Operand execute(Operand op1, Operand op2 ){
-    Operand temp = new Operand(op1.getValue() + op2.getValue());
-    return temp;
+    return (new Operand(op1.getValue()+ op2.getValue()));
     }
 
 }
